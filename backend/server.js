4593d85c0fed76fcc8970a108dafa98b87dadf42
ethen09 <etheny09@gmail.com>
2025-06-16 16:28:21 -0400

    ServerSEtup
    complete

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app= express();
+
+app.get("/products", (req, res) => {
+    res.send("Server is ready nig");
+})
+
+app.listen(5000, () =>{
+    console.log("Server started at http://localhost:5000")
+})
